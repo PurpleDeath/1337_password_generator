707c8426db2e08e1b92dc16fab75e736aef61422
PurpleDeath <mcklink@bsugmail.net>
2015-10-23 21:39:19 -0600

    1337 Pass Phrase Generator
    The most elite hacker pass phrase generator! Combs through a custom
    dictionary in order to ensure that all passwords are the most elite (and
    actually make sense..)!

---
+++ leet pw gen.ps1
@@ -0,0 +1,31 @@
+﻿$leet = @{e=3;o=0;l=1;i='!';s=5; t='7'}
+$rand = new-object System.Random
+$conjunction = "the", "my"
+$words = import-csv dict.csv
+$TextInfo = (Get-Culture).TextInfo
+$numberofpws = 0
+
+$quantity = Read-Host 'H0w m4ny 1337 p4$$W0rd$ d0 y0u n33d? ..pr3$$ q t0 qu!t.'
+    if ($quantity -eq 'q') {break}
+    else{
+Do{
+
+$noun = ($words[$rand.Next(0,$words.Count)]).Noun
+  if ($args[0]) { $noun = $args[0] }
+  $leet.Keys | foreach-object { $skip = $rand.Next(0,4); if ($skip -ne 0){ $noun = $noun.Replace($_,$leet[$_]) } }
+  
+$verb = ($words[$rand.Next(0,$words.Count)]).Verb
+  if ($args[0]) { $verb = $args[0] }
+  $leet.Keys | foreach-object { $skip = $rand.Next(0,4); if ($skip -ne 0){ $verb = $verb.Replace($_,$leet[$_]) } }
+
+$con = ($conjunction[$rand.Next(0,$conjunction.Count)])
+
+$passphrase = ($TextInfo.ToTitleCase($verb) + $TextInfo.ToTitleCase($con) + $TextInfo.ToTitleCase($noun))
+
+Write-Host 'Your new password is:'$passphrase;
+
+$numberofpws++
+
+} while ($numberofpws -lt $quantity)
+} 
+{pause}
